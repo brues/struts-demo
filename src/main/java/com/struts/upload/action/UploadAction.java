3864efd2da77f5d7b9dd508516014a34f6772d0a
guoyb <guoyb@anbi.tv>
2014-07-22 21:49:05 +0800

    增加异步上传图片的例子

--- src/main/java/com/struts/upload/action/UploadAction.java
+++ src/main/java/com/struts/upload/action/UploadAction.java
@@ -1,9 +1,6 @@
 package com.struts.upload.action;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 
 /**
  * Created by guoyibin on 7/22/14.
@@ -13,12 +10,14 @@ public class UploadAction {
 
     private String img;
 
+    private String image;
+
     public String upload(){
         return "upload";
     }
 
     /**
-     * 文件同步上传例子
+     * 文件同步上传例子   success!!!
      * */
     public String uploadImg() throws Exception{
         FileInputStream fi=new FileInputStream(img);
@@ -40,6 +39,31 @@ public class UploadAction {
         return "uploadImg";
     }
 
+
+
+    /**
+     * 文件异步上传例子   成功！！！
+     * */
+    public String ajaxUpload() throws Exception {
+        FileInputStream fi=new FileInputStream(image);
+        BufferedInputStream in=new BufferedInputStream(fi);
+        FileOutputStream fo=new FileOutputStream("src/main/webapp/resources/upload/test.jpg");
+        BufferedOutputStream out=new BufferedOutputStream(fo);
+
+        byte[] buf=new byte[1024];
+        int len=in.read(buf);//读文件，将读到的内容放入到buf数组中，返回的是读到的长度
+        while(len!=-1){
+            out.write(buf, 0, len);
+            len=in.read(buf);
+        }
+
+        out.close();
+        fo.close();
+        in.close();
+        fi.close();
+        return "success";
+    }
+
     public String getImg() {
         return img;
     }
@@ -48,4 +72,11 @@ public class UploadAction {
         this.img = img;
     }
 
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
--- src/main/webapp/upload.jsp
+++ src/main/webapp/upload.jsp
@@ -2,12 +2,29 @@
 <html>
     <head>
         <title></title>
+        <script src="/resources/jquery/jquery.js"></script>
+        <script src="/resources/jquery/jquery.form.js"></script>
+        <script>
+            $(document).ready(function() {
+                $("#ajaxbtn").click(function(){
+                    $("#ajaxform").ajaxSubmit({
+                        url:"/upload/ajaxUpload.action"
+                    });
+                });
+            });
+        </script>
     </head>
     <body>
         <form action="/upload/uploadImg.action" id="form" method="post" enctype="multipart/form-data">
-            <input type="text" value="123" name="str"/>
-            <input name="img" type="file" />
+            <span>同步图片上传</span>
+            <input name="img" type="file" /><br/>
             <input type="submit" value="提交" />
         </form>
+        <br/><br/><br/><br/><br/>
+        <form id="ajaxform" method="post" enctype="multipart/form-data">
+            <span>异步图片上传</span>
+            <input name="image" type="file" />
+        </form>
+        <input id="ajaxbtn" type="button" value="提交" />
     </body>
 </html>
